d7ea6b12a0e198dd4a0472eed960af4d7f557c22
Sal Savage <salsavage@google.com>
2022-08-22 10:35:18 +0100

    tanzanite: props: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,1,16
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x01 -> 1 (Computer)
    MINOR_CLASS: 0x10 -> 16 (Handheld PC/PDA)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is correct
    Change-Id: I0ac8ab5279c299a93074b2bc9d074fa2acdf5078
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,7 @@ TARGET_SCREEN_DENSITY := 420
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Android Verified Boot
