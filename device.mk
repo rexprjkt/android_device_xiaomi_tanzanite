addaa50b8f844d7fb313c278421da07353f7119c
Woomymy <woomy@woomy.be>
2024-10-30 13:43:53 +0100

    tanzanite: Configure bluetooth stack
    Change-Id: Ib067503ccf46ca77021f1454f97beebb078570a6
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -79,6 +79,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl:64
+
 # Boot control (A/B Updates)
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
