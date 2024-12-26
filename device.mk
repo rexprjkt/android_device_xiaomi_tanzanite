c3875077c3fa25b64e32c1b0d1ad8203678efab3
claxten10 <claxten10@gmail.com>
2024-12-26 21:58:51 +0000

    tanzanite: Configure GNSS stack
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
     fstab.mt6789.vendor_ramdisk \
     ueventd.mt6789.rc
 
+# GNSS
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
